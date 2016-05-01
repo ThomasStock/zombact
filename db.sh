2609a91d1e29cb10c36c06e797ff3ebe5e2c1a5d
Thomas Stock <thomasstock1985@gmail.com>
2016-05-01 14:27:32 +0000

    x

---
+++ db.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo su couchdb -c /usr/bin/couchdb
+
+#node c9couch.js
+
+#sudo chmod 777 -R .git/objects this fixed authentication issues
---
+++ gitpush.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git add -A
+git commit -am "x"
+git push
+
+#sudo chmod 777 -R .git/objects this fixed authentication issues
